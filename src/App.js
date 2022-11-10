ce84f740b510e44d39230084a0edada7c7617e1f
VitaliyKhrol <vitaliyx@gmail.com>
2022-11-10 20:11:26 +0200

    sortUser

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
+import React from "react";
+import Greeting from "./components/Greeting";
+import {userData} from "./components/Greeting/userData";
 
-import React from 'react';
-import Greting from './components/Greeting';
-
-class App extends React.Component{
-
-  render(){
-    return (
-      <section>
-        <Greting userName="John"/>
-        <Greting userName="John"/>
-        <Greting userName="John"/>
-        <Greting userName="Josn"/>
-      </section>
-    )
-  }
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        users: userData,
+        isSort: true
+    }
 }
+
+  userMap = () => this.state.users.map((userObj) => <Greeting userName={userObj.name} key={userObj.id}/>);
+
+  sortUsers = () => {
+    const {users, isSort} = this.state;
+    const newUsers = [...users];
+    newUsers.sort((a,b) => (a.name > b.name && isSort) ? 1 : -1);
+    this.setState({
+        users: newUsers,
+        isSort: !isSort
+    })
+}
+
+render () {
+    return (
+        <section>
+            <button onClick={this.sortUsers}>Sorted</button>
+            {this.userMap()}
+        </section>
+    )
+}
+}
+
 export default App;
