21968e40ef170d7717fc0ccab1e58555eb80f363
VitaliyKhrol <vitaliyx@gmail.com>
2022-11-10 14:53:24 +0200

    Greeting

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react';
+import Greting from './components/Greeting';
+
+class App extends React.Component{
+
+  render(){
+    return (
+      <section>
+        <Greting userName="John"/>
+        <Greting userName="John"/>
+        <Greting userName="John"/>
+        <Greting userName="Josn"/>
+      </section>
+    )
+  }
 }
-
 export default App;
